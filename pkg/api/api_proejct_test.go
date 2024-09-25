16514e2c7071b8327afd81a6619e696425c282f9
zhang.tiancheng <zhang.tiancheng@tslsmart.com>
2024-09-25 15:57:31 +0800

    repackage

--- pkg/api/api_proejct_test.go
+++ pkg/api/api_proejct_test.go
@@ -4,7 +4,7 @@ import (
 	"testing"
 
 	"github.com/gookit/goutil/testutil/assert"
-	"github.com/tslsmartztc/cube_project_cli/pkg"
+	pkg "github.com/tslsmartztc/cube_project_cli/pkg/api"
 )
 
 func Test_GetProjWithCode(t *testing.T) {
